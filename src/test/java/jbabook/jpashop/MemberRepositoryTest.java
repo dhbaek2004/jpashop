3dabb12999844fac9dc81414b4b612df1167f295
daehyun.baek <deahyun.baek@tysystems.com>
2021-10-25 21:58:48 +0900

    Commit

---
+++ src/test/java/jbabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,11 @@
+package jbabook.jpashop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@RunWi
+@SpringBootTest
+public class MemberRepositoryTest {
+
+}
